e44b42c8abcfab3dffa5824895586307b2d2d8d1
Isaac Nisillus <thaint@ants.vn>
2017-09-01 17:37:23 +0700

    - Move all source files to app folder - Add sass/scss's configurations

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -190,6 +190,15 @@ module.exports = {
               },
             ],
           },
+          {
+            test: /\.s[ac]ss$/,
+            include: paths.appSrc,
+            loaders: [
+                require.resolve('style-loader'),
+                require.resolve('css-loader'),
+                require.resolve('sass-loader')
+            ]
+          },
           // "file" loader makes sure those assets get served by WebpackDevServer.
           // When you `import` an asset, you get its (virtual) filename.
           // In production, they would get copied to the `build` folder.
--- src/app/Dribbble.test.js
+++ src/app/Dribbble.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Dribbble from './Dribbble';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Dribbble />, div);
 });
